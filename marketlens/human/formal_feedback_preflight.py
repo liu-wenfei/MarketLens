6ec9ccd7cd42f1458ccd53d72b1080e2d6c469c6
liu-wenfei <liuwenfei0406@163.com>
2026-09-02 14:39:24 +0100

    Bump formal feedback preflight identity to v4

--- marketlens/human/formal_feedback_preflight.py
+++ marketlens/human/formal_feedback_preflight.py
@@ -39,7 +39,7 @@ from marketlens.human.formal_feedback_generator import (
 
 
 PREFLIGHT_CONTRACT_VERSION = (
-    "marketlens-formal-feedback-provider-preflight-v3"
+    "marketlens-formal-feedback-provider-preflight-v4"
 )
 PREFLIGHT_LABEL = (
     "NON-FORMAL / PAID PROVIDER PREFLIGHT / "
@@ -47,7 +47,7 @@ PREFLIGHT_LABEL = (
 )
 PREFLIGHT_OUTPUT_RELATIVE = Path(
     "data/marketlens/human/preflight/"
-    "formal_feedback_provider_v3"
+    "formal_feedback_provider_v4"
 )
 EXECUTION_LOCK_NAME = "execution_lock.json"
 SUCCESS_RECEIPT_NAME = "preflight_success.json"
